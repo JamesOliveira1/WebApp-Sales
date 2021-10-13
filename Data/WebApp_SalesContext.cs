fc83e5e7a703c2136e5583d32337634d6ff5fd49
James <escrevaparajames@hotmail.com>
2021-10-13 11:49:08 -0300

    Criando departamento

---
+++ Data/WebApp_SalesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApp_Sales.Models;
+
+namespace WebApp_Sales.Data
+{
+    public class WebApp_SalesContext : DbContext
+    {
+        public WebApp_SalesContext (DbContextOptions<WebApp_SalesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApp_Sales.Models.Department> Department { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApp_Sales.Data;
 
 namespace WebApp_Sales
 {
@@ -33,6 +35,9 @@ namespace WebApp_Sales
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebApp_SalesContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebApp_SalesContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
