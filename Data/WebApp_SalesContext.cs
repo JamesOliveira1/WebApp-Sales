984800c01e35fb1e0a3eded8195302615ba5beab
James <escrevaparajames@hotmail.com>
2021-10-18 14:57:15 -0300

    Update de classes e segunda migration

--- Data/WebApp_SalesContext.cs
+++ Data/WebApp_SalesContext.cs
@@ -14,6 +14,8 @@ namespace WebApp_Sales.Data
         {
         }
 
-        public DbSet<WebApp_Sales.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,8 @@ namespace WebApp_Sales
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebApp_SalesContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebApp_SalesContext")));
+                    options.UseMySql(Configuration.GetConnectionString("WebApp_SalesContext"), builder =>
+            builder.MigrationsAssembly("WebApp_Sales")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
