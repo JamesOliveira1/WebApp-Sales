da9ca6c76a650daf7885ba562fc78b20a049e04e
James <escrevaparajames@hotmail.com>
2021-10-21 16:21:25 -0300

    Alterações no model e formação dados

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -3,25 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.SqlServer.Server;
 
 namespace WebApp_Sales.Models
 {
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Name size should be betwenn 3 and 60 ")]
         public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Required]
+        [Display (Name="Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat (DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
+
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
-
         public Seller() 
         {
 
         }
-
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
@@ -31,17 +50,14 @@ namespace WebApp_Sales.Models
             BaseSalary = baseSalary;
             Department = department;
         }
-
         public void AddSales (SalesRecord sr)
         {
             Sales.Add(sr);
         }
-
         public void RemoveSales(SalesRecord sr)
         {
             Sales.Remove(sr);
         }
-
         public double TotalSales (DateTime initial, DateTime final)
         {
             return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
